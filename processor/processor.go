d542530a503bbbea430271774a479d774d78446b
int16 <int16int8@gmail.com>
2024-09-09 08:22:17 +0000

    fix: Performance Optimize

--- processor/processor.go
+++ processor/processor.go
@@ -8,27 +8,30 @@ import (
 	"github.com/gonebot-dev/gonebot/plugins"
 )
 
+func process(msg messages.IncomingStruct) {
+	result, succ := plugins.TraversePlugins(msg)
+	if !succ {
+		return
+
+	}
+	//Default reply.
+	if result.To == "" {
+		result.To = msg.SenderID
+	}
+	if result.MessageType == "" {
+		result.MessageType = msg.MessageType
+	}
+
+	messages.PushResult(result)
+
+	dResult, _ := json.Marshal(result)
+	log.Printf("Finish Processing: %s\n", dResult)
+}
+
 // The message processor thread.
 func MessageProcessor() {
 	for {
 		msg := messages.PopIncoming()
-
-		result, succ := plugins.TraversePlugins(msg)
-		if !succ {
-			continue
-
-		}
-		//Default reply.
-		if result.To == "" {
-			result.To = msg.SenderID
-		}
-		if result.MessageType == "" {
-			result.MessageType = msg.MessageType
-		}
-
-		messages.PushResult(result)
-
-		dResult, _ := json.Marshal(result)
-		log.Printf("Finish Processing: %s\n", dResult)
+		go process(msg)
 	}
 }
