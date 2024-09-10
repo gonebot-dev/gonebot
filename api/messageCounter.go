5c3127a02847f3fac83bce1c8d71e3ca3f537b33
int16 <int16int8@gmail.com>
2024-09-10 01:55:38 +0000

    feat: Add api: message counter

---
+++ api/messageCounter.go
@@ -0,0 +1,31 @@
+package api
+
+import "sync"
+
+var incomingCount int = 0
+var incomingLock sync.RWMutex
+var resultCount int = 0
+var resultLock sync.RWMutex
+
+func AddIncomingCount() {
+	incomingLock.Lock()
+	incomingCount++
+	incomingLock.Unlock()
+}
+func GetIncomingCount() (result int) {
+	incomingLock.RLock()
+	result = incomingCount
+	incomingLock.RUnlock()
+	return result
+}
+func AddResultCount() {
+	resultLock.Lock()
+	resultCount++
+	resultLock.Unlock()
+}
+func GetResultCount() (result int) {
+	resultLock.RLock()
+	result = resultCount
+	resultLock.Unlock()
+	return result
+}
