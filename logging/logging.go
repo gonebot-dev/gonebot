f7fe180819511a166860269e8116cba4fc4d50cf
Kingcq <404291187@qq.com>
2024-12-11 10:44:55 +0800

    fix: logging trial

--- logging/logging.go
+++ logging/logging.go
@@ -1,7 +1,6 @@
 package logging
 
 import (
-	"fmt"
 	"os"
 	"strings"
 	"time"
@@ -24,8 +23,5 @@ func Logf(level zerolog.Level, callerName string, format string, v ...any) {
 func Init() {
 	// Zerolog configuration
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.DateTime}
-	output.FormatLevel = func(i any) string {
-		return strings.ToUpper(fmt.Sprintf("| %-6s |", i))
-	}
 	Logger = zerolog.New(output).With().Timestamp().Logger()
 }
